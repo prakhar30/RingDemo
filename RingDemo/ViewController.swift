39dffe65e5005cad0a33ca5fc5e3a95b3104d2cf
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2019-03-26 00:17:36 +0530

    added initial files

---
+++ RingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RingDemo
+//
+//  Created by Prakhar Tripathi on 26/03/19.
+//  Copyright © 2019 Prakhar Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
